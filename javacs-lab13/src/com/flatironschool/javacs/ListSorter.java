4c8c9f726d1e048c91fe793c0ef6a62526ee5944
Myanna Harris <myharris13@gmail.com>
2016-07-19 21:09:40 -0700

    Done.

--- javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
+++ javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
@@ -9,6 +9,7 @@ import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Collections;
 
 /**
  * Provides sorting algorithms.
@@ -64,7 +65,34 @@ public class ListSorter<T> {
 	 */
 	public List<T> mergeSort(List<T> list, Comparator<T> comparator) {
         // FILL THIS IN!
-        return null;
+		if(list.size() <2)
+		{
+			return list;
+		}
+		
+		return mergeSortHelper(list, comparator);
+	}
+	
+	private List<T> mergeSortHelper(List<T> list, Comparator<T> comparator) {
+		int mid = list.size() / 2;
+		
+		if(list.size() <2)
+		{
+			return list;
+		}
+		
+		List<T> l1 = mergeSortHelper(list.subList(0, mid), comparator);
+		List<T> l2 = mergeSortHelper(list.subList(mid, list.size()), comparator);
+		
+		return merge(l1, l2, comparator);
+	}
+	
+	private List<T> merge(List<T> l1, List<T> l2, Comparator<T> comparator)
+	{
+		List<T> sorted = new ArrayList<T>(l1);
+		sorted.addAll(l2);
+		insertionSort(sorted, comparator);
+		return sorted;
 	}
 
 	/**
@@ -76,6 +104,18 @@ public class ListSorter<T> {
 	 */
 	public void heapSort(List<T> list, Comparator<T> comparator) {
         // FILL THIS IN!
+		if(!list.isEmpty())
+		{
+			PriorityQueue<T> pq = new PriorityQueue<T>(list.size(), comparator);
+			for(T t : list)
+			{
+				pq.offer(t);
+			}
+			for(int i = 0; i<list.size(); i++)
+			{
+				list.set(i, pq.poll());
+			}
+		}
 	}
 
 	
@@ -90,7 +130,33 @@ public class ListSorter<T> {
 	 */
 	public List<T> topK(int k, List<T> list, Comparator<T> comparator) {
         // FILL THIS IN!
-        return null;
+		if(k < 1)
+		{
+			return null;
+		}
+		PriorityQueue<T> pq = new PriorityQueue<T>(k, comparator);
+		for(T t : list)
+		{
+			if(pq.size() < k)
+			{
+			 pq.offer(t);
+			}
+			else
+			{
+				if(comparator.compare(pq.peek(), t) < 0)
+				{
+					pq.poll();
+					pq.offer(t);
+				}
+			}
+		}
+		List<T> l = new ArrayList<T>();
+		while(pq.peek() != null)
+		{
+			l.add(pq.poll());
+		}
+		
+		return l;
 	}
 
 	
